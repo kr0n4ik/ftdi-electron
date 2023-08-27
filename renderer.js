64c54aa29a1a5fffe03f3369b9d2924b0f7e9f42
kr0n4ik <kr0n4ik@hotmail.com>
2023-08-27 15:14:47 +0300

    add test

--- renderer.js
+++ renderer.js
@@ -1,41 +1,49 @@
 const FTDI = require('ftdi-d2xx')
+const $ = require("jquery")
 
-async function get() {
-    try {
-        // Get the connected devices info list 
-        const list = await FTDI.getDeviceInfoList();
-        console.log(`${list.length} device${list.length>1?'s':''} found:`, list);
-        
-        // If there is at least one device connected
-        if(list.length) {
-      
-          // Try to open the first device from the list
-          device = await FTDI.openDevice("A");
-          console.log(`One device open:`, device);
-    
-          // Setup the device
-          device.setTimeouts(1000, 1000); // set the max TX and RX duration in ms
-          device.purge(FTDI.FT_PURGE_RX); // purge the RX buffer from previous received data
-          device.setBitMode(0x00, 0x02)
-          //device.setBaudRate(115200); // set the UART baud rate (bits per second)
-          //device.setDataCharacteristics(FTDI.FT_BITS_8, FTDI.FT_STOP_BITS_1, FTDI.FT_PARITY_NONE);
-      
-          // Send data from the TXD pin of the device
-          await device.write(Uint8Array.from([0xAA]));
-          console.log(`Data sent successfully.`);
-      
-          // Wait to receive from the RXD pin
-          console.log(`Trying to receive data...`);
-          const response = await device.read(2); // expected response byte length
-          console.log(`${response.byteLength} bytes were received:`, response);
-    
-          // Close the device (device object must then be recreated using openDevice)
-          device.close();
-    
-          // Be careful "not to loose" the device object before closing it, otherwise
-          // the device will likely stay open and you will not be able to re-open it.
-        }
-      } catch (e) {
-        console.error(e);
-      }
+let device = null
+
+async function getData() {
+    const buf = []
+    for (let loop = 0; loop < 5; loop++) {
+        buf.push(0x80)   // команда GPIO для ADBUS
+        buf.push(0x08)   // перевод CS в лог. 1, MOSI и SCLK в лог. 0
+        buf.push(0x0b)   // bit3:CS, bit2:MISO, bit1:MOSI, bit0:SCLK
+    }
+
+    for (let loop = 0; loop < 5; loop++) {
+        buf.push(0x80)   // команда GPIO для ADBUS
+        buf.push(0x00)   // перевод CS в лог. 1, MOSI и SCLK в лог. 0
+        buf.push(0x0b)   // bit3:CS, bit2:MISO, bit1:MOSI, bit0:SCLK
+    }
+
+    await device.write(Uint8Array.from(buf))
 }
+
+$(document).ready(() => {
+    $("#init").click(async () => {
+        device = await FTDI.openDevice("A")
+        device.setTimeouts(1000, 1000)
+        device.setBitMode(0x00, 0x02)
+        await device.write(Uint8Array.from([0xAA]))
+        let response = await device.read(2)
+        if (!response || response[0] != 0xFA) {
+            $("#info").html("Ошибка перехода в MPPSE")
+            device.close()
+            return
+        }
+        await device.write(Uint8Array.from([0x8A, 0x97, 0x8D]))
+        $("#info").html("Перешли в MPPSE")
+    })
+    $("#close").click(() => {
+        if (device) {
+            device.close()
+            $("#info").html("Зарыли соеденение")
+        }
+    })
+    $("#test").click(() => {
+        if (device) {
+            getData()
+        }
+    })
+})
